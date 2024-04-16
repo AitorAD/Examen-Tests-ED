901518760c8b00dbedb0b6c9f0573b35b4279b7a
Aitor Agullo Duque <dam_ada@informatica.mz>
2024-04-16 13:41:26 +0200

    Cambiados los nombres de los tests (Nombres descriptivos).

--- src/es/iesmz/tests/MoneyTest.java
+++ src/es/iesmz/tests/MoneyTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MoneyTest {
     @Test
-    public void prueba1() {
+    public void pruebaEUR_USD() {
         float expected = 28.37f;
 
         Money m = new Money();
@@ -14,7 +14,7 @@ class MoneyTest {
     }
 
     @Test
-    public void prueba2() {
+    public void pruebaGBP_EUR() {
         float expected = 1165.83f;
 
         Money m = new Money();
@@ -22,7 +22,7 @@ class MoneyTest {
     }
 
     @Test
-    public void prueba3() {
+    public void pruebaEUR_GBP() {
         float expected = 201.21f;
 
         Money m = new Money();
@@ -30,7 +30,7 @@ class MoneyTest {
     }
 
     @Test
-    public void prueba4() {
+    public void pruebaUSD_EUR() {
         float expected = 37.51f;
 
         Money m = new Money();
@@ -38,7 +38,7 @@ class MoneyTest {
     }
 
     @Test
-    public void prueba5() {
+    public void pruebaGBP_USD() {
         float expected = 138.49f;
 
         Money m = new Money();
@@ -46,28 +46,28 @@ class MoneyTest {
     }
 
     @Test
-    public void prueba6() {
+    public void pruebaUSD_GBP() {
         float expected = 722.14f;
 
         Money m = new Money();
         assertEquals(expected, m.change(TipoMoneda.USD, TipoMoneda.GBP, 1000.0f), 0.01);
     }
     @Test
-    public void prueba7() {
+    public void pruebaPTS_EUR() {
         float expected = -1f;
 
         Money m = new Money();
         assertEquals(expected, m.change(TipoMoneda.PTS, TipoMoneda.EUR, 100.0f), 0.01);
     }
     @Test
-    public void prueba8() {
+    public void pruebaEUR_PTS() {
         float expected = -1f;
 
         Money m = new Money();
         assertEquals(expected, m.change(TipoMoneda.EUR, TipoMoneda.PTS, 123.3f), 0.01);
     }
     @Test
-    public void prueba9() {
+    public void pruebaCantidadNegativa() {
         float expected = -1f;
 
         Money m = new Money();
