d4c3857f6fe90846be2de76bac8815ad6339d29e
Aitor Agullo Duque <dam_ada@informatica.mz>
2024-04-16 12:27:51 +0200

    Creada Clase Money. Creados Tests Money

---
+++ src/MoneyTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoneyTest {
+    @Test
+    public void prueba1() {
+        float expected = 28.37f;
+        assertEquals(expected, 1);
+    }
+
+    @Test
+    public void prueba2() {
+        float expected = 1165.83f;
+        assertEquals(expected, 1);
+    }
+
+    @Test
+    public void prueba3() {
+        float expected = 201.21f;
+        assertEquals(expected, 1);
+    }
+
+    @Test
+    public void prueba4() {
+        float expected = 37.51f;
+        assertEquals(expected, 1);
+    }
+
+    @Test
+    public void prueba5() {
+        float expected = 138.49f;
+        assertEquals(expected, 1);
+    }
+
+    @Test
+    public void prueba6() {
+        float expected = 722.14f;
+        assertEquals(expected, 1);
+    }
+    @Test
+    public void prueba7() {
+        float expected = -1f;
+        assertEquals(expected, 1);
+    }
+    @Test
+    public void prueba8() {
+        float expected = -1f;
+        assertEquals(expected, 1);
+    }
+    @Test
+    public void prueba9() {
+        float expected = -1f;
+        assertEquals(expected, 1);
+    }
+}
